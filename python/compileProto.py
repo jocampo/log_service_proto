b49cd213162778bf23c8022402fda1c6cfcf6945
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2021-09-12 11:20:45 -0500

    create __init__.py for each output folder

--- python/compileProto.py
+++ python/compileProto.py
@@ -20,7 +20,11 @@ if os.path.exists(proto_dir):
 shutil.copytree(proto_path, proto_dir)
 
 proto_files = []
-for root, _, filenames in os.walk(proto_path):
+proto_out_dirs = set()
+for root, dirname, filenames in os.walk(proto_path):
+    if len(dirname):
+        # We'll need these later
+        proto_out_dirs.update(dirname)
     for filename in fnmatch.filter(filenames, '*.proto'):
         proto_files.append(join(root, filename))
 
@@ -34,3 +38,9 @@ subprocess.run(command, shell=True)
 
 init = open(join(proto_dir, '__init__.py'), 'w+')
 init.close()
+
+# Our output folders need to have an __init__.py file
+for directory in proto_out_dirs:
+    init_path = join(proto_dir, directory)
+    init_py = open(join(init_path, '__init__.py'), 'w+')
+    init_py.close()
